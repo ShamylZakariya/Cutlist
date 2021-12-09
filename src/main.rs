dbd995038525ff8c6d83ea8c4a5eeae8e6122405
Shamyl Zakariya <shamyl@gmail.com>
2021-12-08 20:24:40 -0800

    After a hiatus, we're back.
    - Some cleanup and prep for computing a cutlist

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
 use std::{error::Error, fs};
 
 use structopt::StructOpt;
 use yaml_rust::YamlLoader;
 
-mod specification;
+mod model;
+mod cutlist;
 
 #[derive(StructOpt,Debug)]
 pub struct Options {
     #[structopt(short, long, default_value = "input.yaml")]
     pub input: String,
+
+    #[structopt(short, long)]
+    pub visualize: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
@@ -18,10 +26,13 @@ fn main() -> Result<(), Box<dyn Error>>{
     let input_str = fs::read_to_string(opt.input)?;
     let input_yaml = YamlLoader::load_from_str(&input_str)?;
     if let Some(doc) = input_yaml.first() {
-        let doc = specification::Input::from(doc)?;
-        println!("doc:\n{:#?}", doc);
-    }
+        let doc = model::Input::from(doc)?;
+        let cutlist = cutlist::compute(&doc, 16)?;
 
+        if opt.visualize {
+            // kick off some macroquad vis of the cutlist
+        }
+    }
 
     Ok(())
 }
